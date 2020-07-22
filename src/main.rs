b85ca3cf1de91fa44933032d754073406e95cdee
Tuomas Siipola <tuomas@zpl.fi>
2020-07-22 19:12:18 +0300

    Update copyright

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-// SPDX-FileCopyrightText: 2019 Tuomas Siipola
+// SPDX-FileCopyrightText: 2019-2020 Tuomas Siipola
+// SPDX-FileCopyrightText: 2019-2020 Johannes Siipola
+//
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use clap::{App, Arg};
